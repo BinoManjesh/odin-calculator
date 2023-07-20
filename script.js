67c9754ae9710857d03c72c7d9593994973acec3
BinoManjesh <binomanjesh@gmail.com>
2023-07-20 22:28:17 +0530

    Implement onBackspace and onClear

--- script.js
+++ script.js
@@ -19,9 +19,23 @@ function onEqualPress() {
 }
 
 function onClear() {
-
+    display = "";
+    operand1 = "";
+    operand2 = "";
+    operator = "";
+    hasDot = false;
 }
 
 function onBackspace() {
-
+    if (!display) {
+        return;
+    }
+    display = display.slice(0, -1);
+    if (operand2) {
+        operand2 = operand2.slice(0, -1);
+    } else if (operator) {
+        operator = operator.slice(0, -1);
+    } else {
+        operand1 = operand1.slice(0, -1);
+    }
 }
